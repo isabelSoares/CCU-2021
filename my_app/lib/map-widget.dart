0b6a11818c8638efd93e0c9ce891128edeb3a06f
Famaral97 <filipe97.amaral@gmail.com>
2020-12-05 22:45:51 +0000

    map implementation

--- my_app/lib/map-widget.dart
+++ my_app/lib/map-widget.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/custom-drawer-widget.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapWidget extends StatelessWidget {
   MapWidget();
+
+  GoogleMapController mapController;
+
+    final LatLng _center = const LatLng(38.73622228023147, -9.15344638918027);
+
+    void _onMapCreated(GoogleMapController controller) {
+      mapController = controller;
+    }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Map")),
       drawer: CustomDrawerWidget(),
+      body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 16.5,
+          ),
+        ),
     );
   }
 }
